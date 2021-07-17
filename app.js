fbd50a7f698daa8a9641870087b284f6cbbdc5b4
DESKTOP-N8M2BVP\s7910 <s7910565@gmail.com>
2021-07-18 01:34:19 +0800

    feat: add index & stlye.css

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = 3000
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 
-
+app.use(express.static('public'))
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(express.urlencoded({ extended: true }))
