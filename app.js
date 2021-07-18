b361df7358bacda8f3799fa671c2ca271c08c626
DESKTOP-N8M2BVP\s7910 <s7910565@gmail.com>
2021-07-18 13:34:07 +0800

    feat: add description & functions

--- app.js
+++ app.js
@@ -3,16 +3,26 @@ const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
-const taskData = require('./config/taskData.json')
+const functions = require('./controller/functions')
 
 app.use(express.static('public'))
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main',
+  //handlebarsHelpers
+  helpers: functions
+}))
 app.set('view engine', 'handlebars')
 app.use(express.urlencoded({ extended: true }))
 
 
 app.get('/', (req, res) => {
   res.render('index')
+})
+
+app.post('/', (req, res) => {
+  const occupation = String(req.body.radio)
+  const description = functions.createDescription(req)
+  res.render('index', { description, occupation })
 })
 
 app.listen(port, () => {
